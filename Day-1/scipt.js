6aef36b458cadfab32b5c65564ea41bd95006a17
kalyan017 <kalyanreddy1719@gmail.com>
2026-06-04 16:30:23 +0530

    day-1 tasks

---
+++ Day-1/scipt.js
@@ -0,0 +1,122 @@
+// Task-1:
+
+// let studentName="kalyan";
+// var studentAge=23;
+// const studentCourse="javascript";
+
+// console.log(studentName);
+// console.log(studentAge);
+// console.log(studentCourse);
+
+
+
+// Task-2:
+
+// var company="Stackly";
+// let salary=20000;
+// const country="India";
+
+// console.log(company);
+// console.log(salary);
+// console.log(country);
+
+// company="Google";
+// salary=30000;
+
+// console.log(company);
+// console.log(salary);
+// console.log(country);
+
+
+
+
+// Task-3:
+
+// let username=prompt("what is your name?");
+
+// console.log("welcome"+" "+username);
+
+
+
+// Task-4:
+
+// let confirmed=confirm("Do you know JavaScript?")
+
+// console.log(confirmed);
+
+
+
+
+// Task-5:
+
+// let studentname=prompt("enter your name?");
+// let studentage=prompt("enter your age?");
+// let studentcity=prompt("enter your city?");
+
+// console.log("Name:"+" "+studentname);
+// console.log("Age:"+" "+studentage);
+// console.log("City:"+" "+studentcity);
+
+
+
+
+// Task-6:
+
+// let course="JavaScript";
+
+// alert("Welcome to  JavaScript");
+
+
+
+
+// // Task-7:
+
+// document.writeln("Hello Student<br>");
+// document.writeln("Welcome to JavaScript");
+
+
+
+
+// Task-8:
+
+// let Favoritefood="Chicken Biryani";
+// var Favoritecolor="Black";
+// const favoritemovie="Pushpa";
+
+// console.log("Food:"+" "+Favoritefood);
+// console.log("Color:"+" "+Favoritecolor);
+// console.log("Movie:"+" "+favoritemovie)
+
+
+
+// Task-9:
+
+// var name="kalyan";
+// let age=23;
+// const phone="987654321";
+// let email="kalyan@stackly.com";
+
+// console.log(name);
+// console.log(age);
+// console.log(phone);
+// console.log(email);
+
+
+
+// Task-10:
+
+// let name=prompt("enter your Name?");
+// let course=prompt("enter your course?");
+
+// alert("Hello"+" "+name+","+" "+"Welcome to"+" "+course+" "+"course.")
+
+
+
+
+
+
+
+
+
+
+
