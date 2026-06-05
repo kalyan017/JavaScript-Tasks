cdcfa5e96c79f7fdd061e5999786fdc19303e930
kalyan017 <kalyanreddy1719@gmail.com>
2026-06-05 17:48:35 +0530

    Day-2

---
+++ Day-2/script.js
@@ -0,0 +1,149 @@
+// Task-1:
+
+// let marks=75;
+
+// if(marks>=90){
+// console.log("Grade A");
+// }else if(marks>=75){
+//     console.log("Grade B");
+// }else if(marks>=60){
+//     console.log("Grade C");
+// }
+// else if (marks>=35) {
+//     console.log("Pass");
+// }else{
+//     console.log("Fail");
+// }
+
+// Task-2:
+
+// let salary=25000;
+// let bonus=salary/10;
+// let finalsalary=bonus+salary;
+
+// console.log("Original salary : "+salary);
+// console.log("Bonus : "+bonus);
+// console.log("Final Salary : "+finalsalary);
+
+// Task-3:
+
+// let email = "admin@gmail.com";
+// let password = "12345";
+
+// let useremail=prompt("enter your email?");
+// let userpassword=prompt("enter your password?");
+
+// if(useremail==email && userpassword==password){
+//         console.log("Login Success")
+//     }
+// else{
+//     console.log("Login Failed")
+// }
+
+// Task-4:
+
+// let price=2000;
+
+// if (price>1000) {
+//     let discount=price*20/100;
+//     let finalamount=price-discount;
+//     console.log(finalamount);
+// }
+
+// Task-5:
+
+// let products = ["Laptop","Mobile","Mouse","Keyboard"];
+
+// console.log(products[0]);
+// console.log(products[products.length-1]);
+// console.log(products.length);
+
+// Task-6:
+
+// let employee = {
+//     name:"Naveen",
+//     department:"Development",
+//     salary:30000
+// }
+
+// console.log(employee);
+// console.log(employee.name);
+// console.log(employee.salary);
+
+
+// Task-7:
+
+// let shirt = 500;
+// let pant = 1000;
+// let shoe = 1500;
+
+// let totalbill=shirt+pant+shoe;
+
+// let discount=totalbill/10;
+
+// let finalamount=totalbill-discount;
+
+// console.log(totalbill);
+// console.log(discount);
+// console.log(finalamount);
+
+
+// Task-8:
+
+// let age=19;
+
+// if (age>=18) {
+//     console.log("Eligible for voting")
+// }else{
+//     console.log("Not Eligible")
+// }
+
+
+// Task-9:
+
+// let a=10;
+
+// a++;
+// console.log(a);
+// ++a;
+// console.log(a);
+// a--;
+// console.log(a);
+// --a;
+// console.log(a);
+
+
+// Task-10:
+
+// let isPresent = true;
+// let completedTask = true;
+
+// if(isPresent&&completedTask){
+//     console.log("Eligible for salary")
+// }else{    
+//     console.log("Not Eligible")
+// }
+
+
+// Bonus-Task 
+
+// let employee = {
+//     id:101,
+//     name:"John",
+//     department:"Development",
+//     salary:35000,
+//     experience:2
+// }
+// let bonus=3500;
+// let finalsalary=bonus+employee.salary;
+// let performance="Good"
+
+// console.log("Employee ID : "+employee.id);
+// console.log("Employee Name : "+employee.name);
+// console.log("Department : "+employee.department);
+// console.log("Salary : "+employee.salary);
+// console.log("experience : "+employee.experience+" Years");
+// console.log("Bonus : "+bonus);
+// console.log("Final Salary : "+finalsalary);
+// console.log("performance Status : "+performance);
+
